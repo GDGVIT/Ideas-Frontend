4765cb719036ee158781037cb8f18a329a91fe6d
anirudh <anirudh04mishra@gmail.com>
2022-06-02 21:39:50 +0530

    feat: add PWA functionality

--- project/src/App.svelte
+++ project/src/App.svelte
@@ -1,13 +1,37 @@
 <script>
-  export let name;
+  import { register } from 'register-service-worker';
+
+  register('/service-worker.js', {
+    registrationOptions: { scope: './' },
+    ready(registration) {
+      console.log('Service worker is active.');
+    },
+    registered(registration) {
+      console.log('Service worker has been registered.');
+    },
+    cached(registration) {
+      console.log('Content has been cached for offline use.');
+    },
+    updatefound(registration) {
+      console.log('New content is downloading.');
+    },
+    updated(registration) {
+      console.log('New content is available; please refresh.');
+    },
+    offline() {
+      console.log(
+        'No internet connection found. App is running in offline mode.'
+      );
+    },
+    error(error) {
+      console.error('Error during service worker registration:', error);
+    },
+  });
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  <h1>DSC Ideas Hub</h1>
+  <p>Get things made.</p>
 </main>
 
 <style>
